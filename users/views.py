a572e683624ffc304a1dc8f750e4bc078de8c506
Irshak10 <74018714+Irshak10@users.noreply.github.com>
2021-04-27 20:56:07 +0300

    Add change password form

--- users/views.py
+++ users/views.py
@@ -17,7 +17,6 @@ from django.contrib.auth.tokens import default_token_generator
 from django.utils.encoding import force_bytes
 
 
-
 @unauthenticated_user
 def registration_page(request):
     form = UserRegisterForm()
