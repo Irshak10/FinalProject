bf3506cd0260e048e183a5ec8de953f5b8802cf5
Irshak10 <74018714+Irshak10@users.noreply.github.com>
2021-05-05 19:41:01 +0300

    Little fix: add get_or_create into view.py, Add __str__ to models, delete comments code in decorators.py, delete user if profile is not confirmed

--- users/decorators.py
+++ users/decorators.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import redirect
+from django.contrib.auth.models import User
+
 from users.models import ConfirmedMail
 
 
@@ -12,30 +14,16 @@ def unauthenticated_user(view_func):
     return wrapper_func
 
 
-# def allowed_mails():
-#     def decorator(view_func):
-#         def wrapper_func(request, *args, **kwargs):
-#
-#             group = None
-#             if request.user.groups.exists():
-#                 group = request.user.groups.all()[0].name
-#
-#             if group == 'registered company mails':
-#                 return view_func(request, *args, **kwargs)
-#             else:
-#                 return HttpResponse('Your mail is not confirmed by admin to view this page')
-#         return wrapper_func
-#     return decorator
-
 def allowed_mails():
     def decorator(view_func):
         def wrapper_func(request, *args, **kwargs):
             mails = []
             for i in ConfirmedMail.objects.all():
                 mails.append(i.mails_list)
-            if request.user.email in mails or request.user.username == 'admin':
+            if request.user.email in mails or request.user.is_superuser:
                 return view_func(request, *args, **kwargs)
             else:
+                User.objects.get(email=request.user.email).delete()
                 return HttpResponse('Your mail is not confirmed by admin to view this page')
         return wrapper_func
     return decorator
