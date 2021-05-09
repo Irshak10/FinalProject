7959eeff8e5ccf48aa34ede3d7271ecfe9ee617c
Yevhenii Dits <yevheniidits@gmail.com>
2021-05-09 19:54:53 +0200

    remove 'upload_to' option. now all users images will go to s3 bucket

--- users/models.py
+++ users/models.py
@@ -6,7 +6,7 @@ from django.dispatch import receiver
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
-    profile_picture = models.ImageField(upload_to='static/images', default='static/images/default.png')
+    profile_picture = models.ImageField(default='static/images/default.png')
     accept_email = models.BooleanField(default=True)
 
     def __str__(self):
